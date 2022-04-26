20679bfc74a476cc0a45bcf42c9960f70fa5420c
Lim Jia Yong <64091029+jiayong1008@users.noreply.github.com>
2022-04-26 16:58:51 +0800

    Update file path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sample/portfolio01-hybrid/',
+  base: '/sample-portfolio01-hybrid/',
   plugins: [react()]
 })
